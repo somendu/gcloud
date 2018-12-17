37204f9c9f4a0fb37fbee6f18ee6e5299d58b06c
Somendu <somendu.maiti@gmail.com>
2018-12-17 13:31:59 +0100

    Hello App package change

---
+++ somendu/src/test/java/groupsom/HelloAppEngineTest.java
@@ -0,0 +1,20 @@
+package groupsom;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.somendu.helloapp.HelloAppEngine;
+
+public class HelloAppEngineTest {
+
+  @Test
+  public void test() throws IOException {
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    new HelloAppEngine().doGet(null, response);
+    Assert.assertEquals("text/plain", response.getContentType());
+    Assert.assertEquals("UTF-8", response.getCharacterEncoding());
+    Assert.assertEquals("Hello App Engine!\r\n", response.getWriterContent().toString());
+  }
+}
