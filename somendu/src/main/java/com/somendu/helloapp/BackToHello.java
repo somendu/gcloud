5dcb1baecff8524d32894b87801afbbffc39f973
Somendu <somendu.maiti@gmail.com>
2018-12-19 09:58:02 +0100

    back to hello

---
+++ somendu/src/main/java/com/somendu/helloapp/BackToHello.java
@@ -0,0 +1,34 @@
+package com.somendu.helloapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "BackToHello",
+    urlPatterns = {"/back"}
+)
+public class BackToHello extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/HTML");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("To App Engine!\r\n");
+
+
+
+    
+    
+    PrintWriter out = response.getWriter();
+    out.println("<a href='" + getServletContext().getContextPath() + "/index.html'>To Index Page</a>");
+    
+  }
+}
