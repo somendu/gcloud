d28ff8e8c7ade6d1ed2d42ee608095b6e30758a0
Somendu Maiti <somendu.maiti@gmail.com>
2019-07-15 08:47:35 +0200

    https filter code added

---
+++ Project/src/main/java/com/somendu/common/filter/HttpsFilter.java
@@ -0,0 +1,52 @@
+package com.somendu.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Https Filter for redirecting from http to https
+ * 
+ * @author somendu
+ *
+ */
+public class HttpsFilter implements Filter {
+	private FilterConfig conf;
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		if (req.getRemoteUser() != null && req.getScheme().equals("http")) {
+			String url = "https://" + req.getServerName() + req.getContextPath() + req.getServletPath();
+			if (req.getPathInfo() != null) {
+				url += req.getPathInfo();
+			}
+			resp.sendRedirect(url);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	public FilterConfig getFilterConfig() {
+		return conf;
+	}
+
+	public void setFilterConfig(FilterConfig filterConfig) {
+		conf = filterConfig;
+	}
+
+	public void destroy() {
+	}
+
+	public void init(FilterConfig filterConfig) {
+		conf = filterConfig;
+	}
+}
